2bb8d8b6ae0945b0bc13ed905e02d58d1dc18c2d
dotrider <56666461+dotrider@users.noreply.github.com>
2020-06-17 15:56:09 -0700

    basic functionality set

--- App.js
+++ App.js
@@ -1,114 +1,53 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
+import React, { useState } from 'react';
+//React Native Components
+import { View, StyleSheet, Text, FlatList } from 'react-native'
+import Header from './Components/Header';
+import ListItem from './Components/ListItem'
+import AddItem from './Components/AddItem'
+import { v4 as uuidv4 } from 'uuid'
 
-import React from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
+//Different keywords/events used in React-Native
+//onClick => onPress
+//onChange => onChangeText
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
 
-const App: () => React$Node = () => {
-  return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-    </>
-  );
-};
+const App = () => {
 
+    const [items, setItems] = useState([
+      {id: uuidv4(), text: 'milk'},
+      {id: uuidv4(), text: 'oranges'},
+      {id: uuidv4(), text: 'bread'}
+    ])
+
+    const deleteItem = (id) => {
+      setItems(prevItems => {
+        return prevItems.filter(item => item.id !== id)
+      })
+    }
+
+    const addItem = (text) => {
+      setItems(prevItems => {
+        return [...prevItems, {id: uuidv4, text}]
+      })
+    }
+
+
+  return(
+    <View style={styles.container}>
+      <Header title='Shopping List' />
+      <AddItem addItem={addItem}/>
+      <FlatList data={items} renderItem={({item}) => <ListItem deleteItem={deleteItem} item={item} />}/>
+    </View>
+  )
+}
+
+
+//Styles
 const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
-});
+  container:{
+    flex:1,
+    paddingTop: 60
+  }
 
+})
 export default App;
---
+++ Components/AddItem.js
@@ -0,0 +1,32 @@
+import React, { useState } from 'react'
+import { View, Text, StyleSheet, TextInput, TouchableOpacity } from 'react-native'
+import Icon from 'react-native-vector-icons/dist/FontAwesome'
+
+const AddItem = ({addItem}) => {
+    const [ text, setText ] = useState('')
+
+    return (
+        <View>
+            <TextInput placeholder='Add Item...'
+            style={styles.input} onChangeText={eValue => setText(eValue)}/>
+            <TouchableOpacity onPress={() => addItem(text)}>
+                <Text style={styles.btnText}><Icon name='plus' size={20} /> Add Item</Text>
+            </TouchableOpacity>
+        </View>
+    )
+}
+    const styles = StyleSheet.create({
+        input:{
+            height: 60,
+            padding: 8,
+            fontSize: 18
+        },
+        btnText: {
+            color: 'lightgreen',
+            fontSize: 20,
+            textAlign: 'center'
+        }
+      
+      })
+
+export default AddItem;
